a5bb385595b7a97a23aee2bb306e1963578b3f38
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-05-15 07:07:17 +0200

    submission Bite 11 @ codechalleng.es

--- 11/account.py
+++ 11/account.py
@@ -9,4 +9,37 @@ class Account:
     def balance(self):
         return self.start_balance + sum(self._transactions)
 
-    # add dunder methods below
+    # add dunder methods below
+    def __len__(self):
+        return len(self._transactions)
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __le__(self, other):
+        return self.balance <= other.balance
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    def __ge__(self, other):
+        return self.balance >= other.balance
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def __getitem__(self, position):
+        return self._transactions[position]
+
+    def __add__(self, amount):
+        if not isinstance(amount, int):
+            raise TypeError("The amount must be an integer!")
+        self._transactions.append(amount)
+
+    def __sub__(self, amount):
+        if not isinstance(amount, int):
+            raise TypeError("The amount must be an integer!")
+        self._transactions.append(-amount)
+
+    def __str__(self):
+        return f"{self.name} account - balance: {self.balance}"
