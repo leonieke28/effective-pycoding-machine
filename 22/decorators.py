ae8b831de362e8136f8c16255730c935fe97ebff
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-05-26 09:03:52 +0200

    submission Bite 22 @ codechalleng.es

--- 22/decorators.py
+++ 22/decorators.py
@@ -2,4 +2,11 @@ from functools import wraps
 
 
 def make_html(element):
-    pass
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return f"<{element}>{func(*args, **kwargs)}</{element}>"
+
+        return wrapper
+
+    return decorator
--- 22/test_decorators.py
+++ 22/test_decorators.py
@@ -2,9 +2,9 @@ from decorators import make_html
 
 
 def test_make_html():
-    @make_html('p')
-    @make_html('strong')
-    def get_text(text='I code with PyBites'):
+    @make_html("p")
+    @make_html("strong")
+    def get_text(text="I code with PyBites"):
         return text
 
-    assert get_text() == '<p><strong>I code with PyBites</strong></p>'
+    assert get_text() == "<p><strong>I code with PyBites</strong></p>"
