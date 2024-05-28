3a458bac0a3a9f11bff3b0c59c996424575cf45a
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-05-28 06:23:35 +0200

    submission Bite 24 @ codechalleng.es

--- 24/challenge.py
+++ 24/challenge.py
@@ -2,12 +2,41 @@ from abc import ABC, abstractmethod
 
 
 class Challenge(ABC):
-    pass
+    def __init__(self, number, title):
+        self.number = number
+        self.title = title
+
+    @abstractmethod
+    def verify(self, *args):
+        pass
+
+    @property
+    @abstractmethod
+    def pretty_title(self):
+        pass
 
 
 class BlogChallenge(Challenge):
-    pass
+    def __init__(self, number, title, merged_prs):
+        super().__init__(number, title)
+        self.merged_prs = merged_prs
+
+    def verify(self, pr):
+        return pr in self.merged_prs
+
+    @property
+    def pretty_title(self):
+        return f"PCC{self.number} - {self.title}"
 
 
 class BiteChallenge(Challenge):
-    pass
+    def __init__(self, number, title, result):
+        super().__init__(number, title)
+        self.result = result
+
+    def verify(self, result):
+        return result == self.result
+
+    @property
+    def pretty_title(self):
+        return f"Bite {self.number}. {self.title}"
