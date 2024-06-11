e6874204423dfd2cc9473784ecb25fb25aa45a06
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-06-11 05:14:45 +0200

    submission Bite 40 @ codechalleng.es

--- 40/binary_search.py
+++ 40/binary_search.py
@@ -1,2 +1,13 @@
 def binary_search(sequence, target):
-    pass
+    low = 0
+    high = len(sequence) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if sequence[mid] < target:
+            low = mid + 1
+        elif sequence[mid] > target:
+            high = mid - 1
+        else:
+            return mid
+    return None
