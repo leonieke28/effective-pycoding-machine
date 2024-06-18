ada4317a8543d80c81037da2b92e0242d4f68a17
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-06-18 05:27:24 +0200

    submission Bite 47 @ codechalleng.es

--- 47/password.py
+++ 47/password.py
@@ -6,4 +6,18 @@ used_passwords = set("PassWord@1 PyBit$s9".split())
 
 
 def validate_password(password):
-    pass
+    if not 6 <= len(password) <= 12:
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if sum(char.islower() for char in password) < 2:
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if not any(char in PUNCTUATION_CHARS for char in password):
+        return False
+    if password in used_passwords:
+        return False
+    else:
+        used_passwords.add(password)
+        return True
