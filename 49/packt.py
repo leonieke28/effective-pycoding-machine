6d4d835832698437e59444d142b663433ab1aba0
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2024-06-19 06:38:26 +0200

    submission Bite 49 @ codechalleng.es

--- 49/packt.py
+++ 49/packt.py
@@ -3,12 +3,20 @@ from collections import namedtuple
 from bs4 import BeautifulSoup as Soup
 import requests
 
-PACKT = 'https://bites-data.s3.us-east-2.amazonaws.com/packt.html'
+PACKT = "https://bites-data.s3.us-east-2.amazonaws.com/packt.html"
 CONTENT = requests.get(PACKT).text
 
-Book = namedtuple('Book', 'title description image link')
+Book = namedtuple("Book", "title description image link")
 
 
 def get_book():
     """make a Soup object, parse the relevant html sections, and return a Book namedtuple"""
-    pass
+    soup = Soup(CONTENT, "html.parser")
+    title = soup.find("div", {"class": "dotd-title"}).text.strip()
+    description = (
+        soup.find("div", {"class": "dotd-main-book-summary"}).contents[7].text.strip()
+    )
+    image = soup.find("div", {"class": "dotd-main-book-image"}).find("img")["src"]
+    link = soup.find("div", {"class": "dotd-main-book-image"}).find("a")["href"]
+
+    return Book(title=title, description=description, image=image, link=link)
